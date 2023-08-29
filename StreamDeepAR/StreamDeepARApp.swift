c5d22caec5b9d4ce499fbc48579b56cc8099ae68
Stefan <stefan.blos@gmail.com>
2023-08-29 17:23:08 +0200

    Initial Commit

---
+++ StreamDeepAR/StreamDeepARApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamDeepARApp.swift
+//  StreamDeepAR
+//
+//  Created by Stefan Blos on 29.08.23.
+//
+
+import SwiftUI
+
+@main
+struct StreamDeepARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
