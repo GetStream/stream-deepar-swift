6894d06e8096fcdeeb230cb5a28ff9bce7f911ef
Stefan <stefan.blos@gmail.com>
2023-09-14 17:46:50 +0200

    Basic Stream Setup

--- StreamDeepAR/StreamDeepARApp.swift
+++ StreamDeepAR/StreamDeepARApp.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import StreamVideo
+import StreamVideoSwiftUI
 
 @main
 struct StreamDeepARApp: App {
+    
+    @State var streamVideo: StreamVideoUI?
+    @StateObject var filtersService = FiltersService()
+    
+    private func setupStreamVideo(
+        with apiKey: String,
+        userCredentials: UserCredentials
+    ) {
+        streamVideo = StreamVideoUI(
+            apiKey: apiKey,
+            user: userCredentials.user,
+            token: userCredentials.token,
+            videoConfig: VideoConfig(videoFilters: [filtersService.deepARFilter]),
+            tokenProvider: { result in
+                // Call your networking service to generate a new token here.
+                // When finished, call the result handler with either .success or .failure.
+                result(.success(userCredentials.token))
+            }
+        )
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(filtersService: filtersService)
+                .onAppear {
+                    setupStreamVideo(with: "mcux5wpckxrz", userCredentials: .demoUser)
+                }
         }
     }
 }
---
+++ StreamDeepAR/ViewModel/FiltersService.swift
@@ -0,0 +1,15 @@
+//
+//  FiltersService.swift
+//  StreamDeepAR
+//
+//  Created by Stefan Blos on 14.09.23.
+//
+
+import Foundation
+import StreamVideo
+import StreamVideoSwiftUI
+
+class FiltersService: ObservableObject {
+    
+    var deepARFilter: VideoFilter!
+}
