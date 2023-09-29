d6ee232b3bb798e4d2afa3d97f7da0839240ba3f
Stefan <stefan.blos@gmail.com>
2023-09-29 14:40:03 +0200

    Use correct effects for the project

--- StreamDeepAR/Model/Effect.swift
+++ StreamDeepAR/Model/Effect.swift
@@ -8,24 +8,22 @@
 import Foundation
 
 enum Effect: String, CaseIterable {
-    case VikingHelmet = "viking_helmet"
-    case MakeupLook = "MakeupLook"
-    case SplitViewLook = "Split_View_Look"
-    case EmotionsExaggerator = "Emotions_Exaggerator"
     case EmotionMeter = "Emotion_Meter"
-    case Stallone = "Stallone"
+    case EmotionsExaggerator = "Emotions_Exaggerator"
+    case FireEffect = "Fire_Effect"
     case FlowerFace = "flower_face"
     case GalaxyBackground = "galaxy_background"
-    case Humanoid = "Humanoid"
+    case Hope = "Hope"
+    case MakeupLook = "MakeupLook"
     case NeonDevilHorns = "Neon_Devil_Horns"
     case PingPong = "Ping_Pong"
     case PixelHearts = "Pixel_Hearts"
+    case RayBan = "ray-ban-wayfarer"
     case Snail = "Snail"
-    case Hope = "Hope"
+    case SplitViewLook = "Split_View_Look"
+    case Stallone = "Stallone"
     case VendettaMask = "Vendetta_Mask"
-    case FireEffect = "Fire_Effect"
-    case BurningEffect = "burning_effect"
-    case ElephantTrunk = "Elephant_Trunk"
+    case VikingHelmet = "viking_helmet"
     
     var path: String? {
         return Bundle.main.path(forResource: self.rawValue, ofType: "deepar")
@@ -33,42 +31,38 @@ enum Effect: String, CaseIterable {
     
     var name: String {
         switch self {
-        case .VikingHelmet:
-            return "Viking"
-        case .MakeupLook:
-            return "Makeup"
-        case .SplitViewLook:
-            return "Split View"
-        case .EmotionsExaggerator:
-            return "Emotions"
         case .EmotionMeter:
             return "Meter"
-        case .Stallone:
-            return "Stallone"
+        case .EmotionsExaggerator:
+            return "Emotions"
+        case .FireEffect:
+            return "Fire"
         case .FlowerFace:
             return "Flowers"
         case .GalaxyBackground:
             return "Galaxy"
-        case .Humanoid:
-            return "Humanoid"
+        case .Hope:
+            return "Hope"
+        case .MakeupLook:
+            return "Makeup"
         case .NeonDevilHorns:
             return "Horns"
         case .PingPong:
             return "PingPong"
         case .PixelHearts:
             return "Pixel Hearts"
+        case .RayBan:
+            return "Ray Ban"
         case .Snail:
             return "Snail"
-        case .Hope:
-            return "Hope"
+        case .SplitViewLook:
+            return "Split View"
+        case .Stallone:
+            return "Stallone"
         case .VendettaMask:
             return "Vendetta"
-        case .FireEffect:
-            return "Fire"
-        case .BurningEffect:
-            return "Burning"
-        case .ElephantTrunk:
-            return "Elephant"
+        case .VikingHelmet:
+            return "Viking"
         }
     }
 }
